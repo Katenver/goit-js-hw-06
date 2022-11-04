e98884aa039b1b95d96992f787ffa8863b9e9820
Kateryna Nasurlaieva <107353836+Katenver@users.noreply.github.com>
2022-11-04 15:46:29 +0200

    update 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,21 @@
 
 
-
-
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBtn = document.querySelector(".change-color");
+const spanEl = document.querySelector('.color')
+const bodyEl = document.querySelector('body');
+
+// console.log(bodyEl)
+
+changeBtn.addEventListener('click', changeColor) 
+ function changeColor () {
+  const random = getRandomHexColor();
+  spanEl.textContent = `${random}`;
+  bodyEl.style.background = `${random}`;
+ } 
+  
+
+
+
