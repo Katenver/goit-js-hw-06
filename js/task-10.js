6c5c933ce64af52cc5f97ad817c58e8387b14e1d
Katenver <8711724@ukr.net>
2022-11-06 12:48:18 +0200

    10-

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,14 @@
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+
+console.log(destroyBtn)
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
